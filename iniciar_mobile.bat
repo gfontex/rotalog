514554a8a37cddc74e98164438ca251c1b26759f
Gabriel Fontes <fontexamorim@gmail.com>
2026-09-06 17:50:45 -0300

    fix: ajustar porta do expo para 8082 evitando conflito de porta ocupada

--- iniciar_mobile.bat
+++ iniciar_mobile.bat
@@ -10,5 +10,5 @@ echo 2. Escaneie o QR Code que vai aparecer abaixo com a camera do seu celular.
 echo.
 set PATH=C:\Program Files\nodejs;%PATH%
 cd /d "%~dp0mobile"
-npx expo start
+npx expo start --port 8082
 pause
